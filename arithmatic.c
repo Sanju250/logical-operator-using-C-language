f36821c4567fb5aacd47f63fafc6941dc1c29534
Sanju250 <152637331+Sanju250@users.noreply.github.com>
2023-12-10 07:02:05 -0800

    Add files via upload

---
+++ arithmatic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float a=10.55;
+	float b=20.99;
+	printf("addition two numbers=:%f\n",a+b);
+		printf("substraction two numbers=:%f\n",a-b);
+			printf("multiplication two numbers=:%f\n",a*b);
+				printf("division two numbers=:%f\n",a/b);
+				
+}
---
+++ assingment.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=10;
+	int b=5;
+	int c=20;
+	int d=30;
+	a=a+5;
+	b=b-2;
+	c=c*6;
+	d=d/10;
+	printf("the value of a=%d\n",a);
+	printf("the value of a=%d\n",b);
+	printf("the value of a=%d\n",c);
+	printf("printf value of a=%d\n",d);
+}
